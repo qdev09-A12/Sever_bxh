4e7635883ad783568d235146f1054d969a8dd45a
qdev09-A12 <qdev09@gmail.com>
2025-09-29 22:41:01 +0700

    Fix
    Fix

--- sever.py
+++ sever.py
@@ -44,4 +44,6 @@ def leaderboard():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway sẽ set PORT động
+    app.run(host="0.0.0.0", port=port, debug=False)
